f33f48c7fe12cbf6545e637da0a12dd58c26f167
caleb <caleb@localhost.localdomain>
2024-04-18 16:15:00 +0100

    Added PoC for CVE Lookup Functions

--- ai_module.py
+++ ai_module.py
@@ -62,7 +62,93 @@ def generate_report(data, data2):
 	# print(response.choices[0].message.content)
 	return response.choices[0].message.content
 
+#https://platform.openai.com/docs/assistants/overview?context=with-streaming
+def cve_lookup():
+
+	check_if_apikey_is_set()
+
+	client = OpenAI()
+
+	assistant = client.beta.assistants.create(
+  	name="CVE-Lookup",
+  	instructions="You take in the name and version of a service, and reference the CVE DB files I've given you, to find valid CVEs. Provide links for more information if available",
+  	tools=[{"type": "file_search"}],
+  	model="gpt-4-turbo-2024-04-09",
+	)
+
+	vector_id = store_cve_db(client)
+
+	assistant = client.beta.assistants.update(
+  	assistant_id=assistant.id,
+  	tool_resources={"file_search": {"vector_store_ids": [vector_id]}},
+	)
+
+		# Upload the user provided file to OpenAI
+	message_file = client.files.create(
+	file=open("./nvdcve-1.1-recent.json", "rb"), purpose="assistants"
+	)
+	
+	# Create a thread and attach the file to the message
+	thread = client.beta.threads.create(
+	messages=[
+		{
+		"role": "user",
+		"content": "Windows 11",
+		# Attach the new file to the message.
+		"attachments": [
+			{ "file_id": message_file.id, "tools": [{"type": "file_search"}] }
+		],
+		}
+	]
+	)
+
+	
+	
+	# The thread now has a vector store with that file in its tool resources.
+	print(thread.tool_resources.file_search)
+
+	run = client.beta.threads.runs.create_and_poll(
+    thread_id=thread.id, assistant_id=assistant.id
+	)	
+
+	messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
+
+	message_content = messages[0].content[0].text
+	annotations = message_content.annotations
+	citations = []
+	for index, annotation in enumerate(annotations):
+		message_content.value = message_content.value.replace(annotation.text, f"[{index}]")
+		if file_citation := getattr(annotation, "file_citation", None):
+			cited_file = client.files.retrieve(file_citation.file_id)
+			citations.append(f"[{index}] {cited_file.filename}")
+
+	print(message_content.value)
+	print("\n".join(citations))
+	
+
+#https://nvd.nist.gov/vuln/data-feeds
+def store_cve_db(client):
+		# Create a vector store caled "Financial Statements"
+	vector_store = client.beta.vector_stores.create(name="Financial Statements")
+	
+	# Ready the files for upload to OpenAI 
+	file_paths = ["./nvdcve-1.1-recent.json"]
+	file_streams = [open(path, "rb") for path in file_paths]
+	
+	# Use the upload and poll SDK helper to upload the files, add them to the vector store,
+	# and poll the status of the file batch for completion.
+	file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+	vector_store_id=vector_store.id, files=file_streams
+	)
+	
+	# You can print the status and the file counts of the batch to see the result of this operation. 
+	print(file_batch.status)
+	print(file_batch.file_counts)
+	return vector_store.id
+
+
 # BELOW IS TEST FUNCTIONS - REMOVE LATER
 # data = read_table_by_key("vulns","host","192.168.56.110")
 # generate_report(data)
 
+cve_lookup()
